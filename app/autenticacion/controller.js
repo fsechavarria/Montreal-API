920dd6d3c6a28b51b74a11d728adc3dd1477b694
fsechavarria <fsechavarria@gmail.com>
2018-09-23 20:54:51 -0300

    Pequeños cambios al controller de autenticacion

--- app/autenticacion/controller.js
+++ app/autenticacion/controller.js
@@ -3,36 +3,34 @@ import oracledb from 'oracledb'
 
 /**
  * Autenticar a un usuario.
- * @param {string} req.body.usuario - Usuario del usuario a autenticar.
- * @param {string} req.body.contrasena - Contraseña del usuario a autenticar.
- * @return {json} - 
+ * @param {string} req.body.USUARIO - Usuario del usuario a autenticar.
+ * @param {string} req.body.CONTRASENA - Contraseña del usuario a autenticar.
+ * @return {json} - Usuario encontrado. De lo contrario mensaje de error.
  */
 async function authenticate (req, res) {
-  let bindvars = 
-  { 
-    cursor:  { type: oracledb.CURSOR, dir : oracledb.BIND_OUT }
-  }
-  let result = []
   try {
-    result = await database.executeProcedure('BEGIN SELECTusuario(:cursor); END;', bindvars)
-  } catch (err) {
-    console.error(err.message)
-  }
+    let bindvars = { cursor: { type: oracledb.CURSOR, dir : oracledb.BIND_OUT } }
+    let result = []
+    result = await database.executeGETProcedure('BEGIN SELECTusuario(:cursor); END;', bindvars)
   
-  if (result.length > 0) {
-    let usr
-    result.forEach(user => {
-      if (user.USUARIO === req.body.usuario && user.CONTRASENA === req.body.contrasena) {
-        usr = user
+    if (result.length > 0) {
+      let usr
+      result.forEach(user => {
+        if (user.USUARIO === req.body.USUARIO && user.CONTRASENA === req.body.CONTRASENA) {
+          usr = user
+          return
+        }
+      })
+      if (usr) {
+        res.json({ error: false, data: { usuario: usr } })
+      } else {
+        res.status(404).json({ error: true, data: { message: 'Usuario o contraseña incorrectos' } })
       }
-    })
-    if (usr) {
-      res.json({ error: false, data: { usuario: usr } })
     } else {
       res.status(404).json({ error: true, data: { message: 'Usuario o contraseña incorrectos' } })
     }
-  } else {
-    res.status(404).json({ error: true, data: { message: 'Usuario o contraseña incorrectos' } })
+  } catch (err) {
+    res.status(500).json({ error: true, data: { message: 'Error Interno' } })
   }
 }
 
