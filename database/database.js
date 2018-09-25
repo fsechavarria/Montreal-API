29a8121658a8d94500ec35c54a29e0b7021b98a3
fsechavarria <fsechavarria@gmail.com>
2018-09-25 16:44:18 -0300

    Pequeños cambios al manejo de las consultas

--- database/database.js
+++ database/database.js
@@ -1,12 +1,12 @@
 import oracledb from 'oracledb'
 import dbConfig from './config'
-import { fetchAsync, convert } from './_helpers'
+import { fetchAsync, convert, doClose } from './_helpers'
 
 async function initialize () {
   const pool = await oracledb.createPool(dbConfig.dbPool)
 }
 
-const defaultThreadPoolSize = 4;
+const defaultThreadPoolSize = 10;
 // Increase thread pool size by poolMax
 process.env.UV_THREADPOOL_SIZE = dbConfig.dbPool.poolMax + defaultThreadPoolSize;
 
@@ -59,10 +59,10 @@ function simpleExecute(statement, binds = [], opts = {}) {
 }
 
 /**
- * Metodo para ejecutar procedimientos almacenados que poseen un SELECT
+ * Metodo para ejecutar procedimientos almacenados (SELECT)
  * @param {string} statement - Sentencia SQL para ejecutar el procedimiento
  * @param {object} binds - Parametros del procedimiento
- * @param {int} numRows - Cantidad de columnas por resultado (opcional, por defecto 10)
+ * @param {integer} numRows - Cantidad de columnas por resultado (opcional, por defecto 10)
  * @returns {array} - Arreglo con el resultado del procedimiento.
  */
 async function executeGETProcedure (statement, binds = {}, numRows = 10) {
@@ -75,6 +75,8 @@ async function executeGETProcedure (statement, binds = {}, numRows = 10) {
       await connection.execute(statement, binds, async function (err, result) {
         if (err) {
           await connection.close()
+          err.message = err.message.split('\n')[0]
+          err.message = err.message.replace(new RegExp(dbConfig.dbPool.user + '.', 'ig'), '')
           reject(err.message)
           return
         }
@@ -83,7 +85,9 @@ async function executeGETProcedure (statement, binds = {}, numRows = 10) {
         let resultData = []
         try {
           resultData = await fetchAsync(connection, resultSet, numRows, [])
-          resultData = convert(metaData, resultData)
+          if (resultData) {
+            resultData = convert(metaData, resultData)
+          }
         } catch (err) {
           reject(err)
           return
@@ -97,10 +101,10 @@ async function executeGETProcedure (statement, binds = {}, numRows = 10) {
 }
 
 /**
- * Metodo para ejecutar procedimientos almacenados sin SELECT (Update, Delete, Insert)
+ * Metodo para ejecutar procedimientos almacenados (Update, Delete, Insert)
  * @param {string} statement - Sentencia SQL para ejecutar el procedimiento
  * @param {object} binds - Parametros del procedimiento
- * @returns {int} - ID del objeto insertado
+ * @returns {object} - Objeto insertado
  */
 async function executeProcedure (statement, binds = {}) {
   return new Promise(async (resolve, reject) => {
@@ -112,11 +116,27 @@ async function executeProcedure (statement, binds = {}) {
       await connection.execute(statement, binds, async function (err, result) {
         if (err) {
           await connection.close()
+          err.message = err.message.split('\n')[0]
+          err.message = err.message.replace(new RegExp(dbConfig.dbPool.user + '.', 'ig'), '')
           reject(err.message)
           return
+        } else if (binds.cursor) {
+          try {
+            var metaData = result.outBinds.cursor.metaData
+            let resultSet = result.outBinds.cursor
+            let resultData = await resultSet.getRow()
+            doClose(connection, resultSet)
+            if (resultData) {
+              resultData = convert(metaData, resultData, true)
+            }
+            resolve(resultData)
+          } catch (err) {
+            reject(err)
+          }
+        } else {
+          await connection.close()
+          resolve(result.outBinds)
         }
-        await connection.close()
-        resolve(result.outBinds)
       })
     } catch (err) {
       reject(err)
