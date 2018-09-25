0c0019b0211822542489664d5cb13b3e48a30a60
fsechavarria <fsechavarria@gmail.com>
2018-09-25 16:45:07 -0300

    Creacion rutas PUT y DELETE para contactos

--- app/contacto/controller.js
+++ app/contacto/controller.js
@@ -3,32 +3,17 @@ import oracledb from 'oracledb'
 
 /**
  * Obtener Contactos
- * @param {int} req.params.id - ID del contacto (opcional) .
+ * @param {integer} req.params.id - ID del contacto (opcional).
  * @returns {json} - Contacto(s) encontrado(s). De lo contrario mensaje de error.
  */
 async function GET (req, res) {
   try {
-    let bindvars = { cursor: { type: oracledb.CURSOR, dir : oracledb.BIND_OUT } }
+    const id_contacto = (typeof req.params.id === 'undefined' || isNaN(req.params.id) ) ? 0 : parseInt(req.params.id)
+    let bindvars = { cursor: { type: oracledb.CURSOR, dir : oracledb.BIND_OUT }, id_contacto: id_contacto }
     let result = []
-    result = await database.executeGETProcedure('BEGIN SELECTcontacto(:cursor); END;', bindvars)
+    result = await database.executeGETProcedure('BEGIN SELECTcontacto(:cursor, :id_contacto); END;', bindvars)
     if (result.length > 0) {
-      const id = (typeof req.params.id === 'undefined' || isNaN(req.params.id) ) ? 0 : parseInt(req.params.id)
-      let contacto
-      if (id != 0) {
-        result.forEach(c => {
-          if (parseInt(c.ID_CONTACTO) === id) {
-            contacto = c
-            return
-          }
-        })
-        if (contacto) {
-          res.json({ error: false, data: { contactos: contacto } })
-        } else {
-          res.status(404).json({ error: true, data: { message: 'No se encontró ningún contacto.' } })
-        }
-      } else {
-        res.json({ error: false, data: { contactos: result } })
-      }
+      res.json({ error: false, data: { contactos: result } })
     } else {
       res.status(404).json({ error: true, data: { message: 'No se encontró ningún contacto.' } })
     }
@@ -39,22 +24,22 @@ async function GET (req, res) {
 
 /**
  * Insertar Contacto
- * @param {int} req.body.ID_USUARIO - ID del usuario a quien pertenecerá el contacto.
+ * @param {integer} req.body.ID_USUARIO - ID del usuario a quien pertenecerá el contacto.
  * @param {string} req.body.DESC_CONTACTO - La Descripción del contacto.
  * @param {string} req.body.TIPO_CONTACTO - El Tipo de contacto.
- * @returns {json} - ID del contacto insertado. De lo contrario mensaje de error.
+ * @returns {json} - Todos los datos del contacto insertado. De lo contrario mensaje de error.
  */
 async function POST (req, res) {
   try {
     let bindvars = { 
-      id_contacto: { type: oracledb.NUMBER, dir: oracledb.BIND_OUT },
+      cursor: { type: oracledb.CURSOR, dir : oracledb.BIND_OUT },
       id_usuario: req.body.ID_USUARIO,
       desc_contacto: req.body.DESC_CONTACTO,
-      tipo_contacto: req.body.TIPO_CONTACTO
+      tipo_contacto: req.body.TIPO_CONTACTO,
     }
-    let result = await database.executeProcedure('BEGIN INSERTcontacto(:id_contacto, :id_usuario, :desc_contacto, :tipo_contacto); END;', bindvars)
-    if (result) {
-      res.json({ error: false, data: { id_contacto: result.id_contacto } })
+    let result = await database.executeProcedure('BEGIN INSERTcontacto(:cursor, :id_usuario, :desc_contacto, :tipo_contacto); END;', bindvars)
+    if (result && result.length > 0) {
+      res.json({ error: false, data: { contacto: result[0] } })
     } else {
       res.status(500).json({ error: true, data: { message: 'Error Interno' } })
     }
@@ -63,7 +48,72 @@ async function POST (req, res) {
   }
 }
 
+/**
+ * Actualizar Contacto
+ * @param {integer} req.params.id - ID del contacto a actualizar (opcional).
+ * @param {integer} req.body.ID_USUARIO - ID del usuario dueño del contacto (opcional).
+ * @param {string} req.body.DESC_CONTACTO - Descripción del contacto (opcional).
+ * @param {string} req.body.TIPO_CONTACTO - Tipo de contacto (opcional).
+ * @returns {json} - Objeto con el contacto actualizado.
+ */
+async function PUT (req, res) {
+  try {
+    const id_contacto = (typeof req.params.id  === 'undefined' || isNaN(req.params.id ) ) ? 0 : parseInt(req.params.id)
+    if (id_contacto != 0) {
+      let bindAux = { cursor: { type: oracledb.CURSOR, dir : oracledb.BIND_OUT }, id_contacto: id_contacto }
+      let aux = await database.executeGETProcedure('BEGIN SELECTcontacto(:cursor, :id_contacto); END;', bindAux)
+      if (aux && aux.length > 0) {
+        let bindvars = {
+          cursor: { type: oracledb.CURSOR, dir : oracledb.BIND_OUT },
+          id_contacto: id_contacto,
+          id_usuario: (typeof req.body.ID_USUARIO === 'undefined') ? aux[0].ID_USUARIO : req.body.ID_USUARIO,
+          desc_contacto: (typeof req.body.DESC_CONTACTO === 'undefined') ? aux[0].DESC_CONTACTO : req.body.DESC_CONTACTO,
+          tipo_contacto: (typeof req.body.TIPO_CONTACTO === 'undefined') ? aux[0].TIPO_CONTACTO : req.body.TIPO_CONTACTO
+        }
+        let result = await database.executeProcedure('BEGIN UPDATEcontacto(:cursor, :id_contacto, :id_usuario, :desc_contacto, :tipo_contacto); END;', bindvars)
+        if (result && result.length > 0) {
+          res.json({ error: false, data: { message: 'Contacto Actualizado', contacto: result[0] } })
+        } else {
+          res.status(404).json({ error: true, data: { message: 'Error Interno' } })
+        }
+      } else {
+        res.status(404).json({ error: true, data: { message: 'No se encontró ningún contacto' } })
+      }
+    } else {
+      res.status(404).json({ error: true, data: { message: 'No se encontró ningún contacto' } })
+    }
+  } catch (err) {
+    res.status(500).json({ error: true, data: { message: err } })
+  }
+}
+
+/**
+ * Eliminar Contacto
+ * @param {integer} req.params.id - ID del contacto a eliminar.
+ * @returns {json} - Objeto con el contacto eliminado. De lo contrario mensaje de error.
+ */
+async function DELETE (req, res) {
+  try {
+    const id_contacto = (typeof req.params.id === 'undefined' || isNaN(req.params.id) ) ? 0 : parseInt(req.params.id)
+    if (id_contacto != 0) {
+      let bindvars = { cursor: { type: oracledb.CURSOR, dir : oracledb.BIND_OUT }, id_contacto: id_contacto }
+      let result = await database.executeProcedure('BEGIN DELETEcontacto(:cursor, :id_contacto); END;', bindvars)
+      if (result && result.length > 0) {
+        res.json({ error: false, data: { message: 'Contacto Eliminado', contacto: result[0] } })
+      } else {
+        res.status(404).json({ error: true, data: { message: 'No se encontró ningún contacto' } })
+      }
+    } else {
+      res.status(404).json({ error: true, data: { message: 'No se encontró ningún contacto' } })
+    }
+  } catch (err) {
+    res.status(500).json({ error: true, data: { message: 'Error Interno' } })
+  }
+}
+
 module.exports = {
   GET,
-  POST
+  POST,
+  PUT,
+  DELETE
 }
--- app/contacto/public.routes.js
+++ app/contacto/public.routes.js
@@ -6,5 +6,7 @@ const app = express()
 app.route('/contacto/:id([0-9]+)?')
   .get((req, res) => controller.GET(req, res))
   .post((req, res) => controller.POST(req, res))
+  .put((req, res) => controller.PUT(req, res))
+  .delete((req, res) => controller.DELETE(req, res))
 
 export default app
