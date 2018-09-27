2af398aaee60db3bc1f16f30a0428ffa63686b8e
fsechavarria <fsechavarria@gmail.com>
2018-09-27 18:30:46 -0300

    Creacion de rutas para pais y ciudad

--- app/public.routes.js
+++ app/public.routes.js
@@ -1,11 +1,15 @@
 import routesInicio from './_inicio/routes'
 import routesAutenticacion from './autenticacion/public.routes'
 import routesContacto from './models/contacto/public.routes'
+import routesPais from './models/pais/public.routes'
+import routesCiudad from './models/ciudad/public.routes'
 
 const routes = [
   routesInicio,
   routesAutenticacion,
-  routesContacto
+  routesContacto,
+  routesPais,
+  routesCiudad
 ]
 
 export default routes
