41dbcb07ff11327f147652f59a931273d6b155f4
fsechavarria <fsechavarria@gmail.com>
2018-09-27 20:43:08 -0300

    Se agregan rutas direccion, rol y usuario

--- app/public.routes.js
+++ app/public.routes.js
@@ -3,13 +3,19 @@ import routesAutenticacion from './autenticacion/public.routes'
 import routesContacto from './models/contacto/public.routes'
 import routesPais from './models/pais/public.routes'
 import routesCiudad from './models/ciudad/public.routes'
+import routesDireccion from './models/direccion/public.routes'
+import routesRol from './models/rol/public.routes'
+import routesUsuario from './models/usuario/public.routes'
 
 const routes = [
   routesInicio,
   routesAutenticacion,
   routesContacto,
   routesPais,
-  routesCiudad
+  routesCiudad,
+  routesDireccion,
+  routesRol,
+  routesUsuario
 ]
 
 export default routes
