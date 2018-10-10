983b244f2e5d4dbbee84d610055bd6ac26e54db2
fsechavarria <fsechavarria@gmail.com>
2018-10-10 20:00:30 -0300

    Se crea crud para Personas

---
+++ app/models/persona/controller.js
@@ -0,0 +1,164 @@
+import database from '../../../database/database'
+import oracledb from 'oracledb'
+import { formatDate } from '../_helpers'
+import RutValidation from './rut_validator'
+
+/**
+ * Obtener Postulaciones
+ * @param {integer} req.params.id - ID de la persona. (Opcional) 
+ * @param {integer} req.query.id_direccion - ID de la dirección de la persona. (Opcional)
+ * @param {integer} req.query.id_usuario - ID del usuario al que pertenece la persona. (Opcional)
+ * @param {string} req.query.rut - Rut de la persona. (Opcional)
+ * @param {string} req.query.nombre - Nombre de la persona. (Opcional)
+ * @param {string} req.query.app_paterno - Apellido paterno de la persona. (Opcional)
+ * @param {string} req.query.app_materno - Apellido materno de la persona. (Opcional)
+ * @param {string} req.query.fech_nacimiento - Fecha de nacimiento de la persona. (Opcional)
+ * @returns {json} - Objeto con las personas encontradas
+ */
+async function GET (req, res) {
+	try {
+		let bindvars = {
+			cursor: { type: oracledb.CURSOR, dir : oracledb.BIND_OUT },
+      id_persona: (typeof req.params.id === 'undefined' || isNaN(req.params.id) ) ? undefined : parseInt(req.params.id),
+      id_direccion: (typeof req.query.id_direccion === 'undefined' || isNaN(req.query.id_direccion) || String(req.query.id_direccion).trim().length === 0) ? undefined : parseInt(req.query.id_direccion),
+      id_usuario: (typeof req.query.id_usuario === 'undefined' || isNaN(req.query.id_usuario) || String(req.query.id_usuario).trim().length === 0) ? undefined : parseInt(req.query.id_usuario),
+      rut: (typeof req.query.rut !== 'string' || req.query.rut.trim().length === 0 || !RutValidation(req.query.rut.trim())) ? undefined : RutValidation(req.query.rut.trim()),
+      nombre: (typeof req.query.nombre !== 'string' || req.query.nombre.trim().length === 0) ? undefined : req.query.nombre.trim(),
+      app_paterno: (typeof req.query.app_paterno !== 'string' || req.query.app_paterno.trim().length === 0) ? undefined : req.query.app_paterno.trim(),
+      app_materno: (typeof req.query.app_materno !== 'string' || req.query.app_materno.trim().length === 0) ? undefined : req.query.app_materno.trim(),
+      fech_nacimiento: (typeof req.query.fech_nacimiento !== 'string' || req.query.fech_nacimiento.trim().length === 0 || !formatDate(req.query.fech_nacimiento.trim())) ? undefined : formatDate(req.query.fech_nacimiento.trim()),
+    }
+    if (typeof req.query.rut !== 'undefined' && typeof bindvars.rut === 'undefined') {
+      res.status(400).json({ error: true, data: { message: 'Rut Inválido.' } })
+      return
+    }
+		let result = []
+		result = await database.executeGETProcedure('BEGIN SELECTpersona(:cursor, :id_persona, :id_direccion, :id_usuario, :rut, :nombre, :app_paterno, :app_materno, :fech_nacimiento); END;', bindvars)
+		if (result.length > 0) {
+      res.json({ error: false, data: { persona: result } })
+    } else {
+      res.status(404).json({ error: false, data: { message: 'No se encontró ninguna persona.' } })
+    }
+	} catch (err) {
+		res.status(500).json({ error: true, data: { message: err } })
+	}
+}
+
+/**
+ * Ingresar Postulación
+ * @param {integer} req.body.ID_DIRECCION - ID de la dirección de la persona.
+ * @param {integer} req.body.ID_USUARIO - ID del usuario al que pertenece la persona.
+ * @param {string} req.body.RUT - Rut de la persona.
+ * @param {string} req.body.NOMBRE - Nombre de la persona.
+ * @param {string} req.body.APP_PATERNO - Apellido paterno de la persona.
+ * @param {string} req.body.APP_MATERNO - Apellido materno de la persona.
+ * @param {string} req.body.FECH_NACIMIENTO - Fecha de nacimiento de la persona.
+ * @returns {json} - Objeto con la persona ingresada.
+ */
+async function POST (req, res) {
+  try {
+    let bindvars = {
+      cursor: { type: oracledb.CURSOR, dir : oracledb.BIND_OUT },
+      id_direccion: (typeof req.body.ID_DIRECCION === 'undefined' || isNaN(req.body.ID_DIRECCION) || String(req.body.ID_DIRECCION).trim().length === 0) ? undefined : parseInt(req.body.ID_DIRECCION),
+      id_usuario: (typeof req.body.ID_USUARIO === 'undefined' || isNaN(req.body.ID_USUARIO) || String(req.body.ID_USUARIO).trim().length === 0) ? undefined : parseInt(req.body.ID_USUARIO),
+      rut: (typeof req.body.RUT !== 'string' || req.body.RUT.trim().length === 0 || !RutValidation(req.body.RUT.trim())) ? undefined : RutValidation(req.body.RUT.trim()),
+      nombre: (typeof req.body.NOMBRE !== 'string' || req.body.NOMBRE.trim().length === 0) ? undefined : req.body.NOMBRE.trim(),
+      app_paterno: (typeof req.body.APP_PATERNO !== 'string' || req.body.APP_PATERNO.trim().length === 0) ? undefined : req.body.APP_PATERNO.trim(),
+      app_materno: (typeof req.body.APP_MATERNO !== 'string' || req.body.APP_MATERNO.trim().length === 0) ? undefined : req.body.APP_MATERNO.trim(),
+      fech_nacimiento: (typeof req.body.FECH_NACIMIENTO !== 'string' || req.body.FECH_NACIMIENTO.trim().length === 0 || !formatDate(req.body.FECH_NACIMIENTO.trim())) ? undefined : formatDate(req.body.FECH_NACIMIENTO.trim()),
+    }
+    if (typeof bindvars.rut === 'undefined') {
+      res.status(400).json({ error: true, data: { message: 'Rut Inválido.' } })
+      return
+    }
+    if (bindvars.id_usuario !== undefined && bindvars.id_direccion !== undefined && bindvars.nombre !== undefined &&
+         bindvars.app_paterno !== undefined && bindvars.app_materno !== undefined && bindvars.fech_nacimiento !== undefined) {
+      let result = await database.executeProcedure('BEGIN INSERTpersona(:cursor, :id_direccion, :id_usuario, :rut, :nombre, :app_paterno, :app_materno, :fech_nacimiento); END;', bindvars)
+      if (result && result.length > 0) {
+        res.json({ error: false, data: { persona: result[0] } })
+      } else {
+        res.status(500).json({ error: true, data: { message: 'Error Interno' } })
+      }
+    } else {
+      res.status(400).json({ error: true, data: { message: 'Parámetros Inválidos' } })
+    }
+  } catch (err) {
+    res.status(500).json({ error: true, data: { message: err } })
+  }
+}
+
+/**
+ * Actualizar Postulación
+ * @param {integer} req.body.ID_PERSONA - ID de la persona.
+ * @param {integer} req.body.ID_DIRECCION - ID de la dirección de la persona. (Opcional)
+ * @param {integer} req.body.ID_USUARIO - ID del usuario al que pertenece la persona. (Opcional)
+ * @param {string} req.body.RUT - Rut de la persona. (Opcional)
+ * @param {string} req.body.NOMBRE - Nombre de la persona. (Opcional)
+ * @param {string} req.body.APP_PATERNO - Apellido paterno de la persona. (Opcional)
+ * @param {string} req.body.APP_MATERNO - Apellido materno de la persona. (Opcional)
+ * @param {string} req.body.FECH_NACIMIENTO - Fecha de nacimiento de la persona. (Opcional)
+ * @returns {json} - Objeto con la persona actualizada.
+ */
+async function PUT (req, res) {
+  try {
+		const id_persona = (typeof req.params.id  === 'undefined' || isNaN(req.params.id ) ) ? 0 : parseInt(req.params.id)
+    if (id_persona != 0) {
+      let bindvars = {
+        cursor: { type: oracledb.CURSOR, dir : oracledb.BIND_OUT },
+        id_persona: id_persona,
+        id_direccion: (typeof req.body.ID_DIRECCION === 'undefined' || isNaN(req.body.ID_DIRECCION) || String(req.body.ID_DIRECCION).trim().length === 0) ? undefined : parseInt(req.body.ID_DIRECCION),
+        id_usuario: (typeof req.body.ID_USUARIO === 'undefined' || isNaN(req.body.ID_USUARIO) || String(req.body.ID_USUARIO).trim().length === 0) ? undefined : parseInt(req.body.ID_USUARIO),
+        rut: (typeof req.body.RUT !== 'string' || req.body.RUT.trim().length === 0 || !RutValidation(req.body.RUT.trim())) ? undefined : RutValidation(req.body.RUT.trim()),
+        nombre: (typeof req.body.NOMBRE !== 'string' || req.body.NOMBRE.trim().length === 0) ? undefined : req.body.NOMBRE.trim(),
+        app_paterno: (typeof req.body.APP_PATERNO !== 'string' || req.body.APP_PATERNO.trim().length === 0) ? undefined : req.body.APP_PATERNO.trim(),
+        app_materno: (typeof req.body.APP_MATERNO !== 'string' || req.body.APP_MATERNO.trim().length === 0) ? undefined : req.body.APP_MATERNO.trim(),
+        fech_nacimiento: (typeof req.body.FECH_NACIMIENTO !== 'string' || req.body.FECH_NACIMIENTO.trim().length === 0 || !formatDate(req.body.FECH_NACIMIENTO.trim())) ? undefined : formatDate(req.body.FECH_NACIMIENTO.trim()),
+      }
+      if (typeof req.query.rut !== 'undefined' && typeof bindvars.rut === 'undefined') {
+        res.status(400).json({ error: true, data: { message: 'Rut Inválido.' } })
+        return
+      }
+      let result = await database.executeProcedure('BEGIN UPDATEpersona(:cursor, :id_persona, :id_direccion, :id_usuario, :rut, :nombre, :app_paterno, :app_materno, :fech_nacimiento); END;', bindvars)
+      if (result && result.length > 0 && result.length === 1) {
+        res.json({ error: false, data: { message: 'Persona Actualizada', persona: result[0] } })
+      } else {
+        res.status(404).json({ error: false, data: { message: 'No se encontró ninguna persona' } })
+      }
+    } else {
+      res.status(404).json({ error: false, data: { message: 'No se encontró ninguna persona' } })
+    }
+  } catch (err) {
+    res.status(500).json({ error: true, data: { message: err } })
+  }
+}
+
+/**
+ * Eliminar Persona
+ * @param {integer} req.params.id - ID de la persona.
+ * @returns {json} - Objeto con la persona eliminada.
+ */
+async function DELETE (req, res) {
+	try {
+    const id_persona = (typeof req.params.id === 'undefined' || isNaN(req.params.id) ) ? 0 : parseInt(req.params.id)
+    if (id_persona != 0) {
+      let bindvars = { cursor: { type: oracledb.CURSOR, dir : oracledb.BIND_OUT }, id_persona: id_persona }
+      let result = await database.executeProcedure('BEGIN DELETEpersona(:cursor, :id_persona); END;', bindvars)
+      if (result && result.length > 0) {
+        res.json({ error: false, data: { message: 'Persona Eliminada', persona: result[0] } })
+      } else {
+        res.status(404).json({ error: false, data: { message: 'No se encontró ninguna persona' } })
+      }
+    } else {
+      res.status(404).json({ error: false, data: { message: 'No se encontró ninguna persona' } })
+    }
+  } catch (err) {
+    res.status(500).json({ error: true, data: { message: err } })
+  }
+}
+
+module.exports = {
+	GET,
+	POST,
+	PUT,
+	DELETE
+}
---
+++ app/models/persona/rut_validator.js
@@ -0,0 +1,63 @@
+'use strict'
+
+function RutValidation (Objeto) {
+  if (Objeto != null) {
+    var tmpstr = ''
+    var intlargo = Objeto
+    if (intlargo.length > 0) {
+      var crut = Objeto
+      var largo = crut.length
+      if (largo < 2) {
+        return false
+      }
+      for (var i = 0; i < crut.length; i++) {
+        if (crut.charAt(i) !== ' ' && crut.charAt(i) !== '.' && crut.charAt(i) !== '-') {
+          tmpstr = tmpstr + crut.charAt(i)
+        }
+      }
+      var rut = tmpstr
+      crut = tmpstr
+      largo = crut.length
+      if (largo > 2) {
+        rut = crut.substring(0, largo - 1)
+      } else {
+        rut = crut.charAt(0)
+      }
+      var dv = crut.charAt(largo - 1)
+      if (rut === null || dv === null) {
+        return 0
+      }
+      var dvr = '0'
+      var suma = 0
+      var mul = 2
+      for (i = rut.length - 1; i >= 0; i--) {
+        suma = suma + rut.charAt(i) * mul
+        if (mul === 7) {
+          mul = 2
+        } else {
+          mul++
+        }
+      }
+      var res = suma % 11
+      if (res === 1) {
+        dvr = 'k'
+      } else {
+        if (res === 0) {
+          dvr = '0'
+        } else {
+          var dvi = 11 - res
+          dvr = dvi + ''
+        }
+      }
+      let rutCompleto = rut + '-' + dv.toLowerCase()
+      if (dvr !== dv.toLowerCase() || rutCompleto.length < 9) {
+        return false
+      }
+      return rutCompleto
+    }
+  } else {
+    return false
+  }
+}
+
+export default RutValidation
